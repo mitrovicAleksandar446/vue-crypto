2ea9626854414d01c8acc05e07cefa4d9c62e3b1
Aleksandar Mitrovic <aleksandar.mitrovic@quantox.com>
2019-02-14 18:02:33 +0100

    Added config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 /** @namespace process.env.VUE_APP_BASE_API_URL */
-export const API_ROOT = process.env.VUE_APP_BASE_API_URL;
+export const API_ROOT = process.env.VUE_APP_BASE_API_URL || "http://api.quantox-coin.com/api";
+
+/** @namespace process.env.VUE_APP_JTW_QXC_TOKEN */
+export const JWT_TOKEN_NAME = process.env.VUE_APP_JTW_QXC_TOKEN || "qxc-token";
